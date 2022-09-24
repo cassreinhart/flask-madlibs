ff8225eb9bf988943318130d231ba71c8939f6d7
Cassandra Reinhart <cassandrareinhart13@gmail.com>
2022-09-24 13:17:11 -0500

    change root route name and template name

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ debug = DebugToolbarExtension(app)
 @app.route('/')
 def show_form():
     prompts = story.prompts
-    return render_template('form.html', prompts= prompts)
+    return render_template('index.html', prompts= prompts)
 
 @app.route('/story')
 def show_story():
